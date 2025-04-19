774f848ac229024053d905f163dffe18a313a87d
chandan9648 <chandankkumar156@gmail.com>
2025-04-19 13:43:55 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function sendMessage() {
+    const input = document.getElementById("chatInput");
+    const chatBox = document.getElementById("chatBox");
+    if (input.value.trim() !== "") {
+      const message = document.createElement("div");
+      message.innerHTML = `<strong>You:</strong> ${input.value}`;
+      chatBox.appendChild(message);
+      input.value = "";
+      chatBox.scrollTop = chatBox.scrollHeight;
+    }
+  }
